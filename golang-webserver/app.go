bb51a4d03502125ac255038fcf96bb6ff9dbbebc
Ricardo Lopes Amorim <104678084+amorimricardo@users.noreply.github.com>
2024-08-16 11:16:15 -0300

    Add files via upload
    first commit

---
+++ golang-webserver/app.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	var name, _ = os.Hostname()
+
+	fmt.Fprintf(w, "<h1>This request was processed by host: %s</h1>\n", name)
+}
+
+func main() {
+	fmt.Fprintf(os.Stdout, "Web server started. Listening on port 8080\n")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
